729956443ad7c7cf5cc5f7e3a0b35b4e43b2f393
六面体 <gamersguyu@gmail.com>
2023-05-22 15:35:46 +0800

    rebase

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,4 +1,52 @@
-import { IconButton } from "./button";
-import { ErrorBoundary } from "./error";
+import { useEffect, useState } from "react";
+// import {
+//     HashRouter as Router,
+//     Routes,
+//     Route,
+//     useLocation,
+//   } from "react-router-dom";
+import { getLoginCode } from "../api/login";
 
-import styles from "./login.module.scss";
+const Login = () => {
+  // const router = Route;
+  const [loginCode, setLoginCode] = useState("");
+
+  // useEffect(() => {
+  //     // Check if user is already logged in
+  //     // If yes, redirect to /chat
+  //     const isLoggedIn = false; // Replace with actual check for logged in user
+  //     if (isLoggedIn) {
+  //         Router.push("/chat");
+  //     }
+  // }, []);
+
+  useEffect(() => {
+    // Fetch login code from API
+    const fetchLoginCode = async () => {
+      const data = await getLoginCode();
+      setLoginCode(data.ticket);
+    };
+    fetchLoginCode();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {loginCode ? (
+        <img
+          src={`https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${loginCode}`}
+          alt="Login QR Code"
+        />
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
+};
+export default Login;
