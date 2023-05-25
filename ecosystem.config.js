6e99718a60fd23c49c2fca7d88574198e56c6dea
六面体 <gamersguyu@gmail.com>
2023-05-25 10:54:31 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
       script    : 'yarn',
       args      : 'start',
       interpreter: '/bin/bash',
+      // 应用程序启动模式，这里设置的是 cluster_mode（集群），默认是fork
+      exec_mode: 'cluster_mode',
+      // 应用启动实例个数，仅在cluster模式有效 默认为fork；或者 max
+      instances: 4,
       watch: '.',
       ignore_watch: ['node_modules', 'logs'],
       cwd: '.'
