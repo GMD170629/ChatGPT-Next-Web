84b916bb69163910a3be33de8d8385889f4b646a
六面体 <gamersguyu@gmail.com>
2023-05-25 10:01:24 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,10 @@ module.exports = {
       interpreter_args: 'start',
       watch: '.',
       ignore_watch: ['node_modules', 'logs'],
+      env: {
+        // 指定.env文件的路径
+        PATH: './.env'
+      }
     }]
   }
   
